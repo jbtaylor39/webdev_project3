d1e1400117672f7bb453de55bfb86ce45816c711
Juan_Taylor <jtaylor39@hawk.illinoistech.edu>
2025-11-20 15:58:20 -0600

    index

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-
 function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 10,
-    center: {lat: 41.8386867, lng: -87.6276365},
-    mapId: 
-  });
-}
 
-window.initMap = initMap;
+  const centerMap = { lat: 41.8386867, lng: -87.6276365 }
+
+  const mapOptions = {
+    center: centerMap,
+    zoom: 10
+  }
+
+  const map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
